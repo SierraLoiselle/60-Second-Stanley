1d91b51472a133c2419346fdaabc7dd5a303a875
Bored1234 <lilyxiaohuan@gmail.com>
2018-11-25 21:42:15 -0800

    Food Prefabs and Script

---
+++ Assets/MakeFood.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeFood : MonoBehaviour {
+
+    public Rigidbody HamPrefab;
+
+    private Rigidbody HamInstance; 
+    
+    // Use this for initialization
+	void Start () {
+        HamInstance = Instantiate(HamPrefab);
+        HamInstance.position = new Vector3(0, .05f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
