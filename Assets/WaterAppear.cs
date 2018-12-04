a7d34d3d39d8b9edd5c93c560757e5f7819e5c70
Sierra Loiselle <sierraml2001@gmail.com>
2018-12-04 13:25:18 -0800

    failed water height change script

---
+++ Assets/WaterAppear.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterAppear : MonoBehaviour {
+
+    public GameObject water;
+    private GameObject oil;
+    public GameObject waterbase;
+    public bool IsThereWater;
+    // Use this for initialization
+	void Start () {
+        IsThereWater = false;
+        oil = Instantiate(water);
+        IsThereWater = true;
+
+        //oil.position = new Vector3()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(IsThereWater == true)
+        {
+            waterbase = GetComponent(FluvioWater4");
+            waterbase.transform.position = new Vector3(0, 1, 0);
+        }
+	}
+
+    
+}
