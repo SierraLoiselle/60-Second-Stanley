8e7c886dd3096b4b875b126b98515a704f336338
Lily_xiaohuan <lilyxiaohuan@gmail.com>
2018-12-04 13:38:08 -0800

    food kinda shows up

--- Assets/MakeFood.cs
+++ Assets/MakeFood.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class MakeFood : MonoBehaviour {
 
+    public Rigidbody BreadPrefab;
+    public Rigidbody CakePrefab;
+    public Rigidbody CheesePrefab;
+    public Rigidbody FishPrefab;
     public Rigidbody HamPrefab;
+    public Rigidbody PiePrefab;
+    public Rigidbody RibsPrefab;
+    public Rigidbody SteakPrefab;
+    public Rigidbody StewPrefab;
+    public Rigidbody WMelonPrefab;
 
-    private Rigidbody HamInstance; 
+
+    private Rigidbody FoodInstance;
+    private List<Rigidbody> foodStuff = new List<Rigidbody>();
     
     // Use this for initialization
 	void Start () {
-        HamInstance = Instantiate(HamPrefab);
-        HamInstance.position = new Vector3(0, .05f, 0);
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            FoodInstance = Instantiate(HamPrefab);
+            FoodInstance.position = new Vector3(Random.Range(-3f, 3f), 0, Random.Range(-23f, 23f));
+        }
 	}
 	
 	// Update is called once per frame
