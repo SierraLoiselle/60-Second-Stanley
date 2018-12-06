b41bb1d293020419fa6157b4d8473fa58af26309
Noah Schapera <littleschaps@gmail.com>
2018-12-06 13:21:54 -0800

    Boundary Box triggered sounds
    by noah <3

---
+++ Assets/Scenes/TriggeredAudioScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggeredAudioScript : MonoBehaviour {
+    public AudioClip noise;
+    AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.PlayOneShot(noise, 0.5F);
+    }
+
+}
