5688972023ef73b3cd2c679962e4239344a7f7bb
Lily_xiaohuan <lilyxiaohuan@gmail.com>
2018-12-14 13:35:45 -0800

    Starting on player status

---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+    public float eatTimeGap;
+
+    private float lastEatTime;
+    private int hunger;
+   
+
+	// Use this for initialization
+	void Start () {
+        hunger = 20;
+        lastEatTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+        {
+            lastEatTime = Time.time;
+            if (hunger < 20)
+            {
+                hunger++;
+            }
+            
+        }
+
+        if (eatTimeGap <= Time.time - lastEatTime)
+        {
+            Debug.Log("time to eat");
+            hunger--;
+        }
+		
+	}
+}
