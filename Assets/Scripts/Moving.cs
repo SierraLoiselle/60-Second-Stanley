c513a1fba1b2cbccf096b5fd2fb2ae19f8ba2d0f
Lily_xiaohuan <lilyxiaohuan@gmail.com>
2019-01-25 13:35:31 -0800

    progress

---
+++ Assets/Scripts/Moving.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour {
+    public GameObject camera;
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindGameObjectsWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
