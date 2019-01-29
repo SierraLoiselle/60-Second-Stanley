8b1b01af5059aec9e8adf0906d2a1b78718b7a1c
Lily_xiaohuan <lilyxiaohuan@gmail.com>
2019-01-29 13:38:11 -0800

    progress

--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Moving : MonoBehaviour {
-    public GameObject camera;
-	// Use this for initialization
-	void Start () {
-		camera = GameObject.FindGameObjectsWithTag("MainCamera");
+    public GameObject cameraRigTransformv;
+    public GameObject cameraRigTransformf;
+    public GameObject headTransformv;
+    public GameObject headTransformf;
+    private bool gameMode;
+    // Use this for initialization
+    void Start () {
+
+		if(cameraRigTransformv.activeInHierarchy == true)
+        {
+            gameMode = true;
+            Debug.Log("AHHHHHHHHHH");
+        }
+        else
+        {
+            gameMode = false;
+            Debug.Log("ahhhhhhhhhh");
+        }
     }
 
     // Update is called once per frame
     void Update () {
-		
+		   
+        
+      
 	}
+
+    public void ToCollectionRoom()
+    {
+
+    }
+    public void ToLockedRoom()
+    {
+
+    }
+
+
 }
