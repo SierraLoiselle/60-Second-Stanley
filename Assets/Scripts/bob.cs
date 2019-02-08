f1a92b263f0efee165355f274545fadaa3ae8905
Amelia <aspam300@gmail.com>
2019-02-08 13:21:26 -0800

    It should work now
    ???

---
+++ Assets/Scripts/bob.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChanger : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+    }
+
+    public void StartButton ()
+    {
+        EditorSceneManager.LoadScene("Collection Room");
+    }
+}
