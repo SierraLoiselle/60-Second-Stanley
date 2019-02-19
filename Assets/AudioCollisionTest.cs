b80b6f365d10f6bbf56046b3b61525a7fed4fc40
Unknown <littleschaps@gmail.com>
2019-02-19 13:36:02 -0800

    recreate audio collision file
    new empty object with "collisiontest" script

---
+++ Assets/AudioCollisionTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCollisionTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		public GameObject 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
