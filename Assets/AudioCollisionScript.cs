383c8bfb0af0241b8ee745831304d7d8ee41c6b5
Unknown <littleschaps@gmail.com>
2019-02-21 13:37:13 -0800

    owo

---
+++ Assets/AudioCollisionScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCollisionScript : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject[] foods;
+    private Collider tempFood;
+    public GameObject tile;
+    private Collider tileCollider;
+    private AudioSource owo;
+
+
+    void Start () {
+        if(foods==null)
+        {
+            foods = GameObject.FindGameObjectsWithTag("Food");
+        }
+        if(tile!=null)
+        {
+            tileCollider = tile.GetComponent<Collider>();
+        }
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject food in foods)
+        {
+            tempFood = food.GetComponent<Collider>();
+            owo = food.GetComponent<>
+            if (tempFood.bounds.Intersects(tileCollider.bounds))
+            {
+
+            }
+        }
+	}
+}
